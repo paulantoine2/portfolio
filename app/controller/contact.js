0261b7291ff2eff8faf990e47ab11d851c723a1f
paulantoine2 <pantoine@core-techs.fr>
2017-06-26 23:52:49 +0200

    Contact

--- app/controller/contact.js
+++ app/controller/contact.js
@@ -2,8 +2,17 @@
  * Created by paulantoine on 16/04/2017.
  */
 
-app.controller('ContactController', function($scope, $http){
+app.controller('ContactController', function($scope, $http, $timeout){
   $scope.submitContact = function(){
+    if(!$scope.name) $scope.nameError = true;
+    if(!$scope.email) $scope.emailError = true;
+    if(!$scope.message) $scope.messageError = true;
+    $timeout(function(){
+      $scope.nameError = false;
+      $scope.emailError = false;
+      $scope.messageError = false;
+    }, 10);
+    if($scope.nameError || $scope.emailError || $scope.messageError) return;
     var formData = {
       name: $scope.name,
       email: $scope.email,
@@ -14,7 +23,6 @@ app.controller('ContactController', function($scope, $http){
       url : 'contact.php',
       data: postData,
       headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-
     }).success(function(res){
       console.log(res);
     }).error(function(error) {
