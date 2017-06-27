ad0e5c9fbbe0d7372e74ead5f02e542f05658111
paulantoine2 <pantoine@core-techs.fr>
2017-06-27 23:11:56 +0200

    Contact

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST['myData']){
+if(isset($_POST['myData'])){
     $myData = json_decode($_POST['myData']);
 
     iconv_set_encoding("internal_encoding", "UTF-8");
@@ -13,26 +13,27 @@ if(isset($_POST['myData']){
     $boundary = "-----=".md5(rand());
     $boundary_alt = "-----=".md5(rand());
 
-    $sujet = "Contact Portfolio - ".$name;
+    $sujet = 'Contact Portfolio';
 
-    $header = "From: \".$from.\"<".$from.">".$passage_ligne;
-    $header.= "Reply-to: \".$from.\" <".$from.">".$passage_ligne;
-    $header.= "MIME-Version: 1.0".$passage_ligne;
-    $header.= "Content-Type: multipart/mixed;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+    $header = 'From: '.$name.' <'.$from.'>'.$passage_ligne;
+    $header.= 'Reply-to: '.$from.' <'.$from.'>'.$passage_ligne;
+    $header.= 'MIME-Version: 1.0'.$passage_ligne;
+    $header.= 'Content-Type: multipart/mixed;'.$passage_ligne.' boundary="'.$boundary.'"'.$passage_ligne;
 
-    $message = $passage_ligne."--".$boundary.$passage_ligne;
-    $message.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary_alt\"".$passage_ligne;
+    $message = $passage_ligne.'--'.$boundary.$passage_ligne;
+    $message.= 'Content-Type: multipart/alternative;'.$passage_ligne.' boundary="'.$boundary_alt.'"'.$passage_ligne;
     $message.= $passage_ligne."--".$boundary_alt.$passage_ligne;
 
-    $message.= "Content-Type: text/plain; charset=\"UTF-8\"".$passage_ligne;
+    $message.= 'Content-Type: text/plain; charset="UTF-8\"'.$passage_ligne;
     $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
     $message.= $passage_ligne.$message_txt.$passage_ligne;
 
     $message.= $passage_ligne."--".$boundary_alt.$passage_ligne;
 
-    mail(utf8_decode($mail),utf8_decode($sujet),utf8_decode($message),utf8_decode($header));
-    echo "success";
+    $res = mail(utf8_decode($mail),utf8_decode($sujet),utf8_decode($message_txt),utf8_decode($header));
+    echo $res;
 }
+?>
 
 
 
